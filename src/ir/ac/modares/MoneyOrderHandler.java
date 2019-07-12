b16597cb123aa875223395080337fc0a4a233346
Mohammad Mahdi Mojahed <mmojahed93@gmail.com>
2019-07-12 18:35:32 +0430

    feat: WIP, Adds some functionality to MoneyOrderHandler

--- src/ir/ac/modares/MoneyOrderHandler.java
+++ src/ir/ac/modares/MoneyOrderHandler.java
@@ -4,19 +4,26 @@ import ir.ac.modares.model.IdentityModel;
 import ir.ac.modares.model.MoneyOrderModel;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.io.*;
 import java.math.BigInteger;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class MoneyOrderHandler {
 
-
+    private final BigInteger secretKey = new BigInteger("15454885155544065937824519745029394769131510805818");
     private final BigInteger upperLimit = new BigInteger("2455155546008943817740293915197451784769108058161191238065");
 
-    private MoneyOrderModel[] moneyOrderList = new MoneyOrderModel[100];
+    private final int generalSize = 100;
+    private MoneyOrderModel[] moneyOrderList = new MoneyOrderModel[generalSize];
+    private BigInteger[] encryptedMoneyOrderList = new BigInteger[generalSize];
+
+    private Set<Integer> checkedMoneyOrderIndexes = new HashSet<>();
 
     private BigInteger userId;
     private String amount;
-    private int identityLen=100;
+    private int identityLen = generalSize;
     private IdentityModel[] identityList;
 
 
@@ -26,12 +33,31 @@ public class MoneyOrderHandler {
         this.identityList = new IdentityModel[identityLen];
     }
 
-    public MoneyOrderModel[] createMoneyOrderList() {
+    public MoneyOrderModel[] getMoneyOrderList() {
+        return moneyOrderList;
+    }
+
+    public BigInteger[] getEncryptedMoneyOrderList() {
+        return encryptedMoneyOrderList;
+    }
+
+    public void createMoneyOrderList() {
         for (int i = 0; i < moneyOrderList.length; i++) {
-            moneyOrderList[i] = createMoneyOrder();
+            MoneyOrderModel moneyOrderModel = createMoneyOrder();
+            moneyOrderList[i] = moneyOrderModel;
+            encryptedMoneyOrderList[i] = new BigInteger(serialize(moneyOrderModel)).multiply(secretKey);
         }
 
-        return moneyOrderList;
+    }
+
+    public MoneyOrderModel getDecryptedMoneyOrderModel(int index) throws Exception {
+        if (index < 0 || index >= moneyOrderList.length) {
+            throw new Exception("Index out of range!");
+        }
+        if (checkedMoneyOrderIndexes.size() >= moneyOrderList.length - 1) {
+            throw new Exception("Request limitation!");
+        }
+        return moneyOrderList[index];
     }
 
     private MoneyOrderModel createMoneyOrder() {
@@ -72,5 +98,56 @@ public class MoneyOrderHandler {
                 || randomNumber.compareTo(upperLimit) > 0); // continue until randomNumber is bigger than lowerLimit and smaller than upperLimit
 
         return randomNumber;
+    }
+
+
+    public static byte[] serialize(Object object) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out;
+        byte[] result = null;
+
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(object);
+            out.flush();
+            result = bos.toByteArray();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+    public static Object deserialize(byte[] bytes) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInput in = null;
+        Object result = null;
+
+        try {
+            in = new ObjectInputStream(bis);
+            result = in.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
     }
 }
