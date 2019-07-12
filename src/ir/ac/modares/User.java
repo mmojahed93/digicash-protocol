5d3af9a10d0ee61479ed7f2edc29d269e111d8aa
Mohammad Mahdi Mojahed <mmojahed93@gmail.com>
2019-07-12 18:51:54 +0430

    feat: Creates User class

---
+++ src/ir/ac/modares/User.java
@@ -0,0 +1,9 @@
+package ir.ac.modares;
+
+import java.math.BigInteger;
+
+public class User {
+
+    public static final BigInteger USER_ID_1 = new BigInteger("1");
+    public static final BigInteger USER_ID_2 = new BigInteger("2");
+}
